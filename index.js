d47a6f4d5acbf24a70ebc839f89583b79c85a32a
xdanujj <147969631+xdanujj@users.noreply.github.com>
2025-11-30 13:57:56 +0530

    commit

---
+++ index.js
@@ -0,0 +1,13 @@
+require('dotenv').config()
+const express=require('express')
+const app=express()
+const port =4000;
+
+app.get('/',(req,res)=>{
+    res.send(<h1>Home Page!</h1>);
+})
+
+app.listen(port,(req,res)=>{
+    console.log(`Listening on port ${port}`);
+    
+})
